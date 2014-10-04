b673e6355887e5dc52c5db8d390811576569322b
Ipalibo <ipalibowhyte@gmail.com>
2014-10-04 18:02:49 +0100

    third

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+  //res.render('index', { title: 'Express' });
 });
 
-router.get('/respond', function(req, res) {
+router.post('/respond', function(req, res) {
+
+	res.send('<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Gather timeout="10" finishOnKey="*">
+        <Say>Please enter your pin number and then press star.</Say>
+    </Gather>
+</Response>');
 
 	console.log('i requested');
   //res.render('index', { title: 'Express' });
