2627a09ea3258f33f9e2ab7a76ce0b2b36d350c1
Ipalibo <ipalibowhyte@gmail.com>
2014-10-04 20:16:08 +0100

    commit since last error

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,26 @@ var accountSid = 'AC8b9da60c81b3b9cbf8bc1df0bc1064e9';
 var authToken = "9c2d302bb7a28d47dd395ef38a846557";
 var client = require('twilio')(accountSid, authToken);
 
-router.post('/', function(req, res) {
+client.messages.create({
+	body: "Jenny please?! I love you <3",
+	to: "+447703358144",
+	from: "+14158141829"
+	}, function(err, message) {
+		console.log(message);
+		//process.stdout.write(message.sid);
+});
 
-	res.send('
-		<?xml version="1.0" encoding="UTF-8"?>
-			<Response>
-    			<Gather timeout="10" finishOnKey="*">
-        			<Say>Please enter your pin number and then press star.</Say>
-    			</Gather>
-			</Response>'
-	);
+/* GET home page. */
+router.get('/', function(req, res) {
+
+});
+
+router.post('/respond', function(req, res) {
+
+	res.send('<?xml version="1.0" encoding="UTF-8"?>');
+
+	console.log('i requested');
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
