1caaba298c84a7a5a0d34ca46ee3462bb99a1aac
Ipalibo <ipalibowhyte@gmail.com>
2014-10-04 19:09:15 +0100

    walla ! you can now text :)

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+require("twilio-node/lib");
+
+// Your accountSid and authToken from twilio.com/user/account
+var accountSid = 'AC8b9da60c81b3b9cbf8bc1df0bc1064e9';
+var authToken = "9c2d302bb7a28d47dd395ef38a846557";
+var client = require('twilio')(accountSid, authToken);
+
 /* GET home page. */
 router.get('/', function(req, res) {
 
-  //res.render('index', { title: 'Express' });
 });
 
 router.post('/respond', function(req, res) {
-
-	res.send('<?xml version="1.0" encoding="UTF-8"?>');
 
 	console.log('i requested');
   //res.render('index', { title: 'Express' });
