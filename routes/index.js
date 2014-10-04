8e9d163861a8626d48375bce4463516f2dd81988
Ipalibo <ipalibowhyte@gmail.com>
2014-10-04 19:32:56 +0100

    blah

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,10 @@ var accountSid = 'AC8b9da60c81b3b9cbf8bc1df0bc1064e9';
 var authToken = "9c2d302bb7a28d47dd395ef38a846557";
 var client = require('twilio')(accountSid, authToken);
 
-/* GET home page. */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
 
-});
-
-router.post('/respond', function(req, res) {
-
-	console.log('i requested');
-  //res.render('index', { title: 'Express' });
+	res.send('<Response> <Say>Hello Monkey</Say> </Response>');
+  
 });
 
 module.exports = router;
